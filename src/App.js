9806b1a4fc019b6698eb6b1031d8636dce1dc658
seungho Jeon <urijsh612@gmail.com>
2019-11-04 15:27:48 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import Potato from "./Potato";
+
+// function Food(props) {
+//   console.log(props);// prop 들로 구성된 객체 출력
+//   return <h1>i like {props.favorite}</h1>;
+// }
+function Food({ favorite }) {
+  return <h1>i like {favorite}</h1>;
+}
 
 function App() {
   return (
     <div className="App">
       <h1>안녕!</h1>
-      <Potato />
+      <Food favorite="김치" />
+      <Food favorite="라면" />
+      <Food favorite="삼겹살" />
+      <Food favorite="소고기" />
     </div>
   );
 }
