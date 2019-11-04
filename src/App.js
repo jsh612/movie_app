e1bae2f82f427fc142901e56b96017287eff4aa0
seungho Jeon <urijsh612@gmail.com>
2019-11-04 15:53:41 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -4,18 +4,54 @@ import React from "react";
 //   console.log(props);// prop 들로 구성된 객체 출력
 //   return <h1>i like {props.favorite}</h1>;
 // }
-function Food({ favorite }) {
-  return <h1>i like {favorite}</h1>;
+// function Food({ favorite }) {
+//   return <h1>i like {favorite}</h1>;
+// }
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <h1>안녕!</h1>
-      <Food favorite="김치" />
-      <Food favorite="라면" />
-      <Food favorite="삼겹살" />
-      <Food favorite="소고기" />
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
