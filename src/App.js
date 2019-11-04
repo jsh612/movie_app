6078eb62ba8cf63afa8625384b291279f6ffc26f
seungho Jeon <urijsh612@gmail.com>
2019-11-04 16:08:17 +0900

    #2.3 .map Recap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// 객체 구조분해 할당 이해
 // function Food(props) {
 //   console.log(props);// prop 들로 구성된 객체 출력
 //   return <h1>i like {props.favorite}</h1>;
@@ -8,49 +9,57 @@ import React from "react";
 //   return <h1>i like {favorite}</h1>;
 // }
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} />
-    </div>
-  );
-}
-
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
   },
   {
+    id: 3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
   },
   {
+    id: 4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
   },
   {
+    id: 5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
   }
 ];
 
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <h1>안녕!</h1>
       {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
+        //Component 들 각자는 항상 유니크 해야한다.
+        // 하지만, 내가 한것과 같이 이를 배열로 만들경우 그 유니크성을 잃어 버리므로
+        // key를 주어 유니크하게 만든다.
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
