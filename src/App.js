5da4eb0730683b7cd254ebbf98c77cac9c256abc
seungho Jeon <urijsh612@gmail.com>
2019-11-05 13:57:31 +0900

    #3.0 /3.1 All I  need to know about State

--- src/App.js
+++ src/App.js
@@ -1,88 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-// 객체 구조분해 할당 이해
-// function Food(props) {
-//   console.log(props);// prop 들로 구성된 객체 출력
-//   return <h1>i like {props.favorite}</h1>;
-// }
-// function Food({ favorite }) {
-//   return <h1>i like {favorite}</h1>;
-// }
-
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.8
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 4.5
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.7
+class App extends Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+  render() {
+    //react는 자동적으로 class component의 render메소드를 실행시킨다.
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-];
-
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-// -prop-types
-//   propTypes에 정의해두었으면 React가 해당 props가 propTypes의 자료형과 일치하는지 검사합니다.
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div className="App">
-      <h1>안녕!</h1>
-      {foodILike.map(dish => (
-        //Component 들 각자는 항상 유니크 해야한다.
-        // 하지만, 내가 한것과 같이 이를 배열로 만들경우 그 유니크성을 잃어 버리므로
-        // key를 주어 유니크하게 만든다.
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
 }
 
 export default App;
