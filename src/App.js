f127e8de14fc68a82f7f73302e4548166b7392ee
seungho Jeon <urijsh612@gmail.com>
2019-11-05 14:40:01 +0900

    #3.2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 
 class App extends Component {
   state = {
@@ -11,8 +10,18 @@ class App extends Component {
   minus = () => {
     this.setState(current => ({ count: current.count - 1 }));
   };
+  componentDidMount() {
+    console.log("Component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+  }
   render() {
     //react는 자동적으로 class component의 render메소드를 실행시킨다.
+    console.log("I'm rendering");
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
