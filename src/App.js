65ba7a41e537a306d7351870099bfea731d07183
seungho Jeon <urijsh612@gmail.com>
2019-11-05 15:20:15 +0900

    #4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
   state = {
     isLoading: true,
-    movie: []
+    movies: []
+  };
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
   };
   componentDidMount() {
+    //life cycle method 로써, 마운트된후 실행
     setTimeout(() => {
       this.setState({ isLoading: false });
     }, 5000);
+    this.getMovies();
   }
   render() {
     //react는 자동적으로 class component의 render메소드를 실행시킨다.
