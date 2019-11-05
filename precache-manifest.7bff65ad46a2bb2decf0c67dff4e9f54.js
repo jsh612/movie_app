51347ce56a3b4f0e54921e22e95711a4b17d9158
seungho Jeon <urijsh612@gmail.com>
2019-11-05 17:48:30 +0900

    Updates

---
+++ precache-manifest.7bff65ad46a2bb2decf0c67dff4e9f54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdebebaf6a6438655e3c60eb11d2f6f",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f1e4137b46fdcf34ca8a",
+    "url": "/movie_app/static/css/main.cbdef483.chunk.css"
+  },
+  {
+    "revision": "cbc22d1e9f5e8712be39",
+    "url": "/movie_app/static/js/2.076e7f06.chunk.js"
+  },
+  {
+    "revision": "f1e4137b46fdcf34ca8a",
+    "url": "/movie_app/static/js/main.755feacb.chunk.js"
+  },
+  {
+    "revision": "8971f8699b511f3e262b",
+    "url": "/movie_app/static/js/runtime-main.2890315e.js"
+  }
+]);
